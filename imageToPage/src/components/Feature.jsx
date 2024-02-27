9e6e79a111341dd85bfea5a6830abc67fc33e6f7
saravan-patidar <140431377+saravan-patidar@users.noreply.github.com>
2024-02-27 15:47:10 +0530

    Add files via upload

---
+++ imageToPage/src/components/Feature.jsx
@@ -0,0 +1,23 @@
+const Feature = () => {
+  return (
+    <div className="feature">
+      <h3>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h3>
+      <div>
+        CHEMICALS & PROCESS <span className="pipe">|</span> POWER
+        <span className="pipe">|</span> WATER & WASTE WATER
+        <span className="pipe">|</span> OILS & GAS
+        <span className="pipe">|</span> PHARMA <span className="pipe">|</span>
+        SUGARS & DISTILLERIES <span className="pipe">|</span> PAPER & PULP
+        <span className="pipe">|</span> MARINE & DEFENCE
+        <span className="pipe">|</span> METAL & MINING
+        <span className="pipe">|</span> FOOD & BEVERAGE
+        <span className="pipe">|</span> PETROCHEMICAL & REFINERIES
+        <span className="pipe">|</span> SOLAR <span className="pipe">|</span>
+        BUILDING <span className="pipe">|</span> HVAC
+        <span className="pipe">|</span> FIRE FIGHTING
+        <span className="pipe">|</span> AGRICULTURE & RESIDENTIAL
+      </div>
+    </div>
+  );
+};
+export default Feature;
---
+++ imageToPage/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "../assests/logo.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={Logo} alt="logo" />
+    </div>
+  );
+};
+export default Header;
